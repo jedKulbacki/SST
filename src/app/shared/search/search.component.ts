3541ed278aa554b87711ae4ecc5db43f258dbf13
Jedidiah Kulbacki <jedidiahkulbacki@gmail.com>
2019-09-09 15:12:34 -0400

    Updated the search bar to work with debouncing. This will start searching for the stock code after waiting 1.5 seconds after the last user input is received.

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { Observable } from 'rxjs';
+import { Component, OnInit} from "@angular/core";
+import { Observable, Subject } from "rxjs";
+import { debounceTime, distinctUntilChanged, map } from "rxjs/operators";
+import { SearchBar } from "tns-core-modules/ui/search-bar";
 
 @Component({
     selector: "search",
@@ -8,7 +10,32 @@ import { Observable } from 'rxjs';
     providers: []
 })
 export class SearchComponent{
-    onTextChange(args){
-        Observable.create()
+
+    searchUpdate: Subject<string>;
+    constructor(){
+        this.searchUpdate = new Subject<string>();
+        
+    }
+    
+    ngAfterViewInit(){
+        this.searchUpdate.pipe(
+            debounceTime(1500),
+            distinctUntilChanged())
+            .subscribe(value => {
+                console.log('you got bounced');
+                this.onSubmit(value);
+            });
+    }
+
+    onSubmit(args){
+        let searchBar = <SearchBar>args.object;
+        console.log('submitted ', searchBar.text);
+        searchBar.dismissSoftInput();
+    }
+
+    onClear(args){
+        let searchBar = <SearchBar>args.object;
+        searchBar.text = "";
+        searchBar.dismissSoftInput();
     }
 }
