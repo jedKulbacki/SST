85512eca999816b7318e9a055b0b5b7c02bd1830
jedKulbacki <jedidiahkulbacki@gmail.com>
2023-03-25 17:47:17 -0400

    Migrate
    Large migration to ns 8

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
 
 import { AppModule } from "./app/app.module";
+import 'zone.js';
 
 // A traditional NativeScript application starts by initializing global objects,
 // setting up global CSS rules, creating, and navigating to the main page.
@@ -10,4 +11,7 @@ import { AppModule } from "./app/app.module";
 // A NativeScript Angular app needs to make both paradigms work together,
 // so we provide a wrapper platform object, platformNativeScriptDynamic,
 // that sets up a NativeScript application and can bootstrap the Angular framework.
-platformNativeScriptDynamic().bootstrapModule(AppModule);
+runNativeScriptAngularApp({
+    appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
+  });
+  
